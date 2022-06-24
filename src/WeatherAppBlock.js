db8f4a65d8a4a52e23e7d2bf0044ae62e124c107
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-06-24 15:55:23 +0100

    Added loading icon

--- src/WeatherAppBlock.js
+++ src/WeatherAppBlock.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import ReactLoading from "react-loading";
 import CurrentLocationRow from "./CurrentLocationRow";
 import CurrentWeatherRow from "./CurrentWeatherRow";
 import DaylyForecastRow from "./DaylyForecastRow";
@@ -21,25 +22,21 @@ export default function WeatherAppBlock(props) {
   useEffect(() => {
     function setCurrentWeatherAndLocation(response) {
       setWeatherData(getWeatherData(response));
-
       setLocationData({
         cityName: response.data.name,
         country: fullCountryName(response.data.sys.country),
         timezone: response.data.timezone,
       });
-
       let apiKey = "294b1233d0859b30eceddba0fee44100";
       let lat = response.data.coord.lat;
       let lon = response.data.coord.lon;
       let apiUrlCurrentWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`;
       axios.get(apiUrlCurrentWeather).then(setForecastWeather);
     }
-
     function setForecastWeather(response) {
       let list = getForecastWeather(response);
       setDailyForecastList(list);
     }
-
     let apiKey = "294b1233d0859b30eceddba0fee44100";
     let urlAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(urlAPI).then(setCurrentWeatherAndLocation);
@@ -79,7 +76,15 @@ export default function WeatherAppBlock(props) {
     );
   } else {
     return (
-      <div className="WeatherAppBlock main-background text-color-mostly-cloudy background-color-mostly-cloudy">
+      <div className="WeatherAppBlock d-flex flex-column justify-content-between main-background text-color-mostly-cloudy background-color-mostly-cloudy">
+        <div className="align-self-center mt-auto mb-auto">
+          <ReactLoading
+            type="bubbles"
+            color="#001d6e"
+            height={"20vmin"}
+            width={"20vmin"}
+          />
+        </div>
         <SearchRow
           saveExampleCity={saveExampleCity}
           saveEnteredCityName={saveEnteredCityName}
